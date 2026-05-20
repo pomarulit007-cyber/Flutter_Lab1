468c06a8cd00f7b990a4bab30450ae7ca399ae37
pomarulit007-cyber <poma.rulit007@gmail.com>
2026-05-20 18:42:41 +0300

    em

---
+++ bin/todo.dart
@@ -0,0 +1,27 @@
+import 'package:todo_app/todo_app.dart' as todo_app;
+
+void main(List<String> arguments) {
+
+}
+
+class Todo {
+  static int _counter = 0;
+
+  final int id;
+  String title;
+  bool isDone;
+
+  Todo(this.title) : id = ++_counter, isDone = false;
+
+  @override
+  String toString() {
+    String status
+    if (isDone) {
+      status = "[x]";
+    } else {
+      status = "[ ]";
+    }
+
+    return "$status $id. $title";
+  }
+}
