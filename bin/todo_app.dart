54c2d433a6a23dc915812dcd049bb14b356e9da0
pomarulit007-cyber <poma.rulit007@gmail.com>
2026-05-20 18:29:57 +0300

    learn dart

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,5 +1,43 @@
 import 'package:todo_app/todo_app.dart' as todo_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${todo_app.calculate()}!');
+  String name = 'Артем';
+  String? name2 = null;
+
+  var count = 0;
+  var title = 'Учить Rust';
+
+  const appName = 'TodoApp';
+
+  print('$name, $name2, $count, $title, $id, $appName');
+
+  List<String> tags = ['институт', 'дом'];
+  Map<String, dynamic> data = {'key': 'value', 'num': 13};
+  Set<int> ids = {1,2,3};
+
+  dynamic anything = 42;
+  anything = 'now a string';
+
+  String greet(String name) => 'Hello, $name!';
+
+  void printTodo({required String title, bool done = false}) {
+    print('${done ? '+' : '-'} $title');
+  }
+  printTodo(title: 'Купить молоко');
+  printTodo(title: 'Проверить код', done: true);
+
+  String repeat(String s, [int times = 2]) => s * times;
+}
+
+class Todo {
+  final int id;
+  String title;
+  bool isDone;
+
+  Todo({required this.id, required this.title, this.isDone = false});
+
+  Todo.empty() : id = 0, title = '', isDone = false;
+
+  @override
+  String toString() => '${isDone ? '+' : '-'} [$id] $title';
 }
